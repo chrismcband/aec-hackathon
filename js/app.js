3252a7dd77edd5bc0970e028ef07f6318b96533f
Christopher McDonald <chrismcband@gmail.com>
2015-07-18 18:07:34 +0100

    Added views for options, choice information and switching between them.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,129 @@
 (function (){
+
   var App = {};
 
+  $(function(){
+    var data = {
+      choices: [
+        {
+          name: "Plan 1",
+          description: "Plan option 1",
+          type: "plan",
+          cost: 1000
+        },
+        {
+          name: "Plan 2",
+          description: "Plan option 2",
+          type: "plan",
+          cost: 1200
+        },
+        {
+          name: "Roof 1",
+          description: "Roof option 1",
+          type: "roof",
+          cost: 2300
+        },
+        {
+          name: "Roof 2",
+          description: "Roof option 2",
+          type: "roof",
+          cost: 2139
+        },
+        {
+          name: "Windows 1",
+          description: "Windows option 1",
+          type: "window",
+          cost: 1000
+        },
+        {
+          name: "Windows 2",
+          description: "Windows option 2",
+          type: "window",
+          cost: 1200
+        }
+      ]
+    };
 
-  var data = {
-    choices: [
-      {
-        name: "Floor 1",
-        type: "Floor"
-      },
-      {
-        name: "Floor 2",
-        type: "Floor"
-      },
-      {
-        name: "Floor 3",
-        type: "Floor"
+    var dataTypes = _(data.choices).groupBy(function(item){
+      return item.type;
+    });
+
+    App.designChoices = new Backbone.Collection(data.choices, {parse: true});
+
+    var planChoices = new Backbone.Collection(App.designChoices.where({type: "plan"}), {parse: true});
+    var roofChoices = new Backbone.Collection(App.designChoices.where({type: "roof"}), {parse: true});
+    var windowChoices = new Backbone.Collection(App.designChoices.where({type: "window"}), {parse: true});
+
+    var collectionTabMap = {
+      'plan': planChoices,
+      'roof': roofChoices,
+      'window': windowChoices
+    };
+
+    var BaseView = Backbone.View.extend({
+      render: function(){
+        var data = this.model ? this.model.toJSON() : {};
+        this.$el.html(this.template(data));
+        return this;
       }
-    ]
-  };
+    });
 
-  App.BaseView = Backbone.View.extend({
-    render: function(){
-      this.$el.html(this.template(this.model.toJSON()));
-    }
+    var TabView = BaseView.extend({
+      el: ".design-choices-tabs",
+      events: {
+        'click a': function(e){
+          this.trigger('tabSelect', $(e.currentTarget).attr('href').substr(1));
+        }
+      },
+      render: function(){
+
+      }
+    });
+
+    var OptionsView = BaseView.extend({
+      template: Handlebars.compile($('#options-template').html()),
+      events: {
+        'click input': 'optionSelected'
+      },
+      optionSelected: function(e){
+        var optionName = $(e.currentTarget).val();
+        var selectedOption = this.collection.find(function(option){
+          return option.get('name') == optionName;
+        });
+        this.trigger('optionSelected', selectedOption);
+      },
+      render: function(){
+        if (this.collection) {
+          this.$el.html(this.template(this.collection.toJSON()));
+        }
+        return this;
+      }
+    });
+
+    var ChoiceInfoView = BaseView.extend({
+      template: Handlebars.compile($('#choice-info-template').html())
+    });
+
+    var choiceInfoView = new ChoiceInfoView();
+    $('#choice-info').html(choiceInfoView.render().el);
+
+    App.optionsView = new OptionsView({
+      collection: planChoices
+    });
+    App.optionsView.$el.appendTo("#options-panel");
+    App.optionsView.render();
+    App.optionsView.on('optionSelected', function(option){
+      var choiceInfoView = new ChoiceInfoView({model: option});
+      $('#choice-info').html(choiceInfoView.render().el);
+    });
+
+
+    App.tabView = new TabView();
+    App.tabView.on('tabSelect', function(tab){
+      App.optionsView.collection = collectionTabMap[tab];
+      App.optionsView.render();
+    });
   });
-
-  App.TabView = App.BaseView.extend({
-    template: Handlebars.compile($('#design-tabs-template').html())
-  });
-
-  App.OptionView = Backbone.View.extend({
-    template: Handlebars.compile($('#option-template').html())
-  });
-
-  var dataTypes = _(data.choices).groupBy(function(item){
-    return item.type;
-  });
-
-
 
   window.App = App;
 })();
